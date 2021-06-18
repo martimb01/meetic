6dc281ff9c60f828d8852585bfe60f305aca8ce6
Martim Bento <martim.souto.bento@gmail.com>
2021-06-18 18:51:59 +0100

    Initial Commit

---
+++ src/main/java/com/meetic/pt/Meetic/MeeticApplication.java
@@ -0,0 +1,13 @@
+package com.meetic.pt.Meetic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeeticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeeticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meetic/pt/Meetic/MeeticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetic.pt.Meetic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeeticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
